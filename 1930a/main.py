2eb004fb74ae1774c0b6f7017711057c424ba395
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-01-15 11:09:17 +1100

    Add files via upload

---
+++ 1930a/main.py
@@ -0,0 +1,27 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+a = II()
+for _ in range(a):
+    b = II()
+    L = sorted(MII())
+    ans = 0
+    for i in range(b):
+        ans += L[i*2]
+    print(ans)
