e55054a00e9c0b93c8e99bbd71e23a92f535b7b3
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-02-06 22:36:33 +1100

    Add files via upload

---
+++ 49a/main.py
@@ -0,0 +1,29 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+s = I()
+a = len(s) - 1
+while not s[a].isalpha():
+    a -= 1
+
+if s[a].lower() in {'a', 'e', 'i', 'o', 'u', 'y'}:
+    print("YES")
+else:
+    print("NO")
