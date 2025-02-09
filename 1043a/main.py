370d316ca0f7d42aed1a4530d3da83b2e3ed95d3
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-02-09 21:50:57 +1100

    Add files via upload

---
+++ 1043a/main.py
@@ -0,0 +1,28 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+a = II()
+L = LII()
+k = max(L)
+c = sum(L)
+while k * a - c <= c:
+    k += 1
+print(k)
---
+++ 954a/main.py
@@ -0,0 +1,29 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+s = I()
+i = ans = 0
+while i < a:
+    if s[i:i+2] == 'UR' or s[i:i+2] == 'RU':
+        i += 1
+    ans += 1
+    i += 1
+print(ans)
