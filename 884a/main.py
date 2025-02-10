bad23ee67a35f4e851a94c1dba307ec3e04141ae
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-02-10 22:26:13 +1100

    Add files via upload

---
+++ 884a/main.py
@@ -0,0 +1,28 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a, b = MII()
+k = 0
+L = LII()
+i = 0
+while k < b:
+    k += 86400 - L[i]
+    i += 1
+print(i)
---
+++ 976a/main.py
@@ -0,0 +1,25 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+s = I()
+
+m, n = s.count('1'), s.count('0')
+print('1' + '0'*n if m > 0 else '0')
