0b0c46dacdd7f4004b1f911a8bea5c3a1129059b
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-02-10 22:27:43 +1100

    Add files via upload

---
+++ 1765b/main.py
@@ -0,0 +1,34 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+a = II()
+for _ in range(a):
+    b = II()
+    s = I()
+    if b % 3 == 2:
+        print("NO")
+        continue
+    for i in range(1, b, 3):
+        if s[i] != s[i+1]:
+            print("NO")
+            break
+    else:
+        print("YES")
