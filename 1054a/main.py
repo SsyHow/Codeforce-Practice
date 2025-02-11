3439a64f391fc750e6a75f2e45c8ed077a763f1c
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-02-11 18:40:10 +1100

    Add files via upload

---
+++ 1054a/main.py
@@ -0,0 +1,26 @@
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+x, y, z, t1, t2, t3 = MII()
+
+m  = abs(x - y) * t1
+n = (abs(x - z) + abs(x - y)) * t2 + 3 * t3
+print("YES" if m >= n else "NO")
